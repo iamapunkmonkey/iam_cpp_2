b70f56be52387483f27a99fb7aecfb1709488c4b
Jordan Max <jordan.max@loop.colum.edu>
2014-03-02 23:17:36 -0600

    official project1 commit
    committing project 1 for Neil and Jordan

---
+++ PokerVersion2/PokerVersion2/Main.cpp
@@ -0,0 +1,15 @@
+#include "Aesthetics.h"
+
+
+//
+
+int main()
+{
+
+	Aesthetics a;
+	system("cls");
+	a.SetWindow(100, 55);
+
+	a.header();
+	a.MainMenu();
+}
---
+++ PokerVersion2/PokerVersion2/Poker.h
@@ -0,0 +1,32 @@
+#ifndef POKER_H
+#define POKER_H
+
+
+
+class Poker
+{
+public:
+	void createdeck(vector<string>& deck);
+	void shuffle(vector<string>& deck);
+	void playercard(vector<string>& deck);
+	void cpucard(vector<string>&deck);
+	void tablecards(vector<string>& deck, int);
+	vector<string> suits(vector<string>, string);
+	void createhand(vector<string> hand, int, int);
+	void createtable(vector<string>, int, int);
+	void DrawingCards(string, vector<string>, int, int, int);
+	void drawcpucard(vector<string>, int, int);
+	void drawcpucard2(vector<string>, int, int);
+	void drawdefaultcpu(vector<string>, int, int);
+	void drawcpudefault(string, vector<string>, int, int, int);
+	void playertest(vector<string>, vector<string>);
+	void playersuit(vector<string>);
+	void playerval(vector<string>);
+	void computertest(vector<string>, vector<string>);
+	void computersuit(vector<string>);
+	void computerval(vector<string>);
+
+	
+};
+
+#endif
